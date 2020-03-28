d478c76ff1d325f158a5b638778d5f20d56a0be8
PanosTsouv <Panostsouv95@gmail.com>
2020-03-28 16:31:21 +0200

    Change arguments of methods register and unregister

--- ConsumerApp/src/Consumer.java
+++ ConsumerApp/src/Consumer.java
@@ -1,5 +1,5 @@
 public interface Consumer extends Node{
-	void register(int brokerID, ArtistName artistName);
-	void disconnect(int brokerID, ArtistName artistName);
+	void register();
+	void unregister();
 	void playData(ArtistName artistName, Value musicFile);
 }
