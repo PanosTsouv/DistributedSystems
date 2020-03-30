0553adca5e31cab1d97e3a2761222cf0b292912f
PanosTsouv <Panostsouv95@gmail.com>
2020-03-31 02:13:26 +0300

    Delete updateNodes() method

--- BrokerApp/src/Node.java
+++ BrokerApp/src/Node.java
@@ -1,13 +1,11 @@
 import java.io.IOException;
 import java.util.ArrayList;
 
-public interface Node{
-	
+public interface Node
+{
 	ArrayList<ArrayList<String>> brokersInfo = new ArrayList<>();
-	
 	void init();
 	ArrayList<ArrayList<String>> getBrokersInfo();
 	void connect() throws IOException;
 	void disconnect();
-	void updateNodes();
 }
