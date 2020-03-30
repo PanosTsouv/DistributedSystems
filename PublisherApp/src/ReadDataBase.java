9fd82ac6da8c76ec246020737c91c8b027c57847
PanosTsouv <Panostsouv95@gmail.com>
2020-03-31 02:01:07 +0300

    Remove debug calls and white spaces

--- PublisherApp/src/ReadDataBase.java
+++ PublisherApp/src/ReadDataBase.java
@@ -1,56 +1,59 @@
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
-
 import com.mpatric.mp3agic.*;
-
-
-public class ReadDataBase {
+public class ReadDataBase{
     private String path;
     private char startChar;
     private char endChar;
 
-    public ReadDataBase(String path, char startChar, char endChar) {
+    public ReadDataBase(String path, char startChar, char endChar)
+    {
         this.path = path;
-        System.out.println(this.path);
         this.startChar = startChar;
         this.endChar = endChar;
-
     }
 
-    public ArrayList<ArrayList<String>> readthePathOfMusicFiles() {
+    public ArrayList<ArrayList<String>> readthePathOfMusicFiles()
+    {
         File file = new File(path);
         String[] names = file.list();// all directories inside dataset1 direcory
         ArrayList<ArrayList<String>> pathOfMusicFiles = new ArrayList<>();
         String artistName = "";
         String trackName = "";
         Mp3File song = null;
-        for (int i = 0; i < names.length; i++) {
-            
+        for (int i = 0; i < names.length; i++) 
+        {
             File file1 = new File(path + '/' + names[i]);
-            
-            if (file1.isDirectory()) {
+            if (file1.isDirectory())
+            {
                 String[] fileNames = file1.list();
-               
                 for (String fileName : fileNames)// all files in directory
                 {
-                    if (fileName.contains(".mp3") && !(fileName.charAt(0) == '.')) {
-                        try {
+                    if (fileName.contains(".mp3") && !(fileName.charAt(0) == '.'))
+                    {
+                        try
+                        {
                             song = new Mp3File(path + '/' + names[i] + '/' + fileName);
-                        } catch (UnsupportedTagException | InvalidDataException | IOException e) {
+                        }
+                        catch (UnsupportedTagException | InvalidDataException | IOException e)
+                        {
                             e.printStackTrace();
                         }
-                        if (song!=null && song.hasId3v1Tag()) {
+                        if(song!=null && song.hasId3v1Tag())
+                        {
                             ID3v1 id3v1Tag = song.getId3v1Tag();
                             artistName = id3v1Tag.getArtist();
                             trackName = id3v1Tag.getTitle();
                         }
-                        if (song!=null && song.hasId3v2Tag()) {
+                        if(song!=null && song.hasId3v2Tag())
+                        {
                             ID3v2 id3v2tag = song.getId3v2Tag();
                             artistName = id3v2tag.getArtist();
                             trackName = id3v2tag.getTitle();
                         }
-                        if(artistName != null && artistName.length() >= 1){
+                        if(artistName != null && artistName.length() >= 1)
+                        {
                             if(artistName.charAt(0) >= startChar && artistName.charAt(0) <= endChar)
                             {
                                 ArrayList<String> songAttributes = new ArrayList<>();
@@ -64,11 +67,6 @@ public class ReadDataBase {
                 }
             }
         }
-        /*debug
-        for (ArrayList<String> element : pathOfMusicFiles)
-        {
-            System.out.println("path: " + element.get(0) + " Artist: " + element.get(1) + " Song: " + element.get(2)); 
-        }*/
         return pathOfMusicFiles;
     }
 }
