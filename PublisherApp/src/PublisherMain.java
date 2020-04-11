0f74dca2345db1947aabb52b91976f9e1d9ce1ce
PanosTsouv <Panostsouv95@gmail.com>
2020-04-11 03:11:19 +0300

    Change main

--- PublisherApp/src/PublisherMain.java
+++ PublisherApp/src/PublisherMain.java
@@ -2,7 +2,7 @@ public class PublisherMain{
     public static void main(String[] args)
     {
         String path = System.getProperty("user.dir") + "/dataset1";
-        PublisherNode PB = new PublisherNode("1", "4321", "4501", "192.168.247.113", path, 'A', 'J');
+        PublisherNode PB = new PublisherNode("1", "4321", "4501", "192.168.1.3", path, 'A', 'J');
 
         new Thread(){
             public void run()
@@ -14,6 +14,12 @@ public class PublisherMain{
         PB.init();
         PB.getBrokerList();
         PB.calculateBrokerArtistMap();
-        PB.sendInitialDataToBrokers();
+        for(int i = 0; i <  PB.getBrokersInfo().size(); i++)
+        {
+            PB.setServerIP(PB.getBrokersInfo().get(i).get(2));
+            PB.setPort(PB.getBrokersInfo().get(i).get(1));
+            PB.init();
+            PB.connect();
+        }
     }
 }
