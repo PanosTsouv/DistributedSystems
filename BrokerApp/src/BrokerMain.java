bd9ee0cd3303a49e2c6f552505b52e8a9c632f69
PanosTsouv <Panostsouv95@gmail.com>
2020-04-11 20:24:20 +0300

    Use args input

--- BrokerApp/src/BrokerMain.java
+++ BrokerApp/src/BrokerMain.java
@@ -2,7 +2,7 @@ public class BrokerMain
 {
     public static void main(String[] args) 
     {
-        BrokerNode broker = new BrokerNode("4321", "1", 2);
+        BrokerNode broker = new BrokerNode(args[0], args[1], Integer.parseInt(args[2]));
         new Thread()
         {
             public void run()
