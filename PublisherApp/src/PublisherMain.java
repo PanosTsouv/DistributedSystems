ed3bcb3c8d9414b41ad94be7bf3413e419a8deda
PanosTsouv <Panostsouv95@gmail.com>
2020-04-11 20:17:33 +0300

    Use args input

--- PublisherApp/src/PublisherMain.java
+++ PublisherApp/src/PublisherMain.java
@@ -2,7 +2,7 @@ public class PublisherMain{
     public static void main(String[] args)
     {
         String path = System.getProperty("user.dir") + "/dataset1";
-        PublisherNode PB = new PublisherNode("1", "4321", "4501", "192.168.1.3", path, 'A', 'J');
+        PublisherNode PB = new PublisherNode(args[0], args[1], args[2], args[3], path, args[4].charAt(0), args[5].charAt(0));
 
         new Thread(){
             public void run()
