364ee587e5bd1e8ed08c467bdf2feeec5c173a2c
PanosTsouv <Panostsouv95@gmail.com>
2020-04-13 09:40:30 +0300

    Add new feature online-offline mode

--- ConsumerApp/src/ConsumerMain.java
+++ ConsumerApp/src/ConsumerMain.java
@@ -1,31 +1,41 @@
 public class ConsumerMain {
 
+    //@param args[4] : Consumer email
     //@param args[0] : Consumer username
     //@param args[1] : Consumer password
     //@param args[2] : server IP
     //@param args[3] : server port
-    //@param args[4] : Consumer email
     public static void main(String[] args) {
         ConsumerNode consumer = new ConsumerNode(args[0], args[1], args[2], args[3], args[4]);
-        consumer.login();
-        consumer.init();
-        consumer.connect();
-        consumer.receiveInfoObject();
-        consumer.findTheRightBroker();
-        consumer.songRequest();
-        if(!consumer.sendUserLoginInfo())
+        if(consumer.getOnline())
         {
-            System.out.println("Invalid login");
-            System.exit(1);
+            consumer.setDownloadedList();
+            consumer.login();
+            consumer.init();
+            consumer.connect();
+            consumer.receiveInfoObject();
+            consumer.findTheRightBroker();
+            consumer.songRequest();
+            if(!consumer.sendUserLoginInfo())
+            {
+                System.out.println("Invalid login");
+                System.exit(1);
+            }
+            if (consumer.getUserChoice().equals("0")) 
+            {
+                consumer.userSelectLive();
+            }
+            else 
+            {
+                consumer.userSelectOffline();
+            }
+            consumer.unregister();
         }
-        if (consumer.getUserChoice().equals("0")) 
+        else
         {
-            consumer.userSelectLive();
+            consumer.setDownloadedList();
+            consumer.getSongNameFromUser();
+            consumer.checkDownloaded();
         }
-        else 
-        {
-            consumer.userSelectOffline();
-        }
-        consumer.unregister();
     }
 }
